03a23723e4e77700fd76930b500135b178a8e977
Ali Fadavi <alifadavi.1381117@gmail.com>
2025-09-25 16:54:49 +0330

    fix(aiogram3.7): use DefaultBotProperties(parse_mode=HTML)

--- bot.py
+++ bot.py
@@ -10,6 +10,8 @@ from aiogram.types import Message, CallbackQuery, BufferedInputFile
 from aiogram.enums import ParseMode
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.filters import CommandStart
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 
 import httpx
 import io
@@ -25,7 +27,7 @@ if not BOT_TOKEN:
     raise RuntimeError("❌ BOT_TOKEN در env تعریف نشده")
 
 # ------------------ Aiogram ------------------
-bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
 
 # ------------------ DB ------------------
